6b3cc4b172bd35786283aa16af684278ac7fca31
khansameerm <sameerkkan331@gmail.com>
2025-09-22 01:23:28 +0500

    final deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"
 
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,12 +11,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 //middleware
-app.use(express.json())       //enables me to use req.body to fetch values in notesController by parsing JSON Bodies: req.body
+if(process.env.NODE_ENV !== "production"){
+       //enables me to use req.body to fetch values in notesController by parsing JSON Bodies: req.body
 app.use(cors({
   origin: "http://localhost:5173"
 }))
+}
+app.use(express.json())
 app.use(rateLimiter)
 
 // app.use((req, res, next) => {
@@ -24,6 +29,16 @@ app.use(rateLimiter)
 // });
 
 app.use("/api/notes", noteRoutes);
+
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
